cbd2e90924bd21077faeb5e4de02edcc10ecd089
Sadixo <manuelgusella13@gmail.com>
2024-12-11 18:30:58 +0100

    Tolto funzione con libreria esterna dal calcoloPosizione

--- calcoloPosizione/calcoloPosizione.py
+++ calcoloPosizione/calcoloPosizione.py
@@ -1,19 +1,8 @@
 import clickhouse_connect
 import json
-from geopy.distance import geodesic
 import time
 
-time.sleep(3)
-
-def calcolo_distanza(utenti, poi):
-    for utente in utenti:
-        for single_poi in poi:
-            #long e lat del poi e dell'utente
-            d_poi=(single_poi[1], single_poi[2])
-            d_user=(utente[5], utente[6])
-
-            if(geodesic(d_user,d_poi).meters <= 50):
-                print("\nLa distanza tra "+utente[0]+" "+utente[1]+" e il poi "+single_poi[0]+" è minore di 50 metri!\n")
+#time.sleep(3)
 
 
 def distanza(utenti):
@@ -71,26 +60,9 @@ ON
 ''').result_rows
 
 # Lettura dei dati dalla tabella punto_interesse JOIN categoria
-#poi_categorie = client.query('''
-#SELECT
-#    pi.nome,
-#    pi.lon,
-#    pi.lat,
-#    pi.indirizzo,
-#    c.categoria
-#FROM 
-#    default.punto_interesse AS pi
-#INNER JOIN
-#    default.categoria AS c 
-#ON
-#    pi.id = c.punto_interesse
-#''').result_rows
-
 #query per prendere già i poi nel raggio di 50 metri dall'utente
 
 # Chiudere il client
 client.close()
 
 distanza(utenti)
-
-#calcolo_distanza(utenti, poi_categorie)
